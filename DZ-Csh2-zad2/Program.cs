f8de0f72ed72fd7b4bfc2fee5c5b361587178f2a
Inna_IT <leto2077@gmail.com>
2023-02-13 21:54:38 +0300

    вывод третьей цифры числа

---
+++ DZ-Csh2-zad2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.Write("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string aText = Convert.ToString(a);
+if (aText.Length > 2)
+{
+    System.Console.WriteLine($"Третья цифра числа: {aText[2]}");
+}
+else
+{
+    System.Console.WriteLine("Третья чифра числа отсутствует");
+}
