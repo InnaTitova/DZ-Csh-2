fc9b6818f7fba38a7fbc0a2d0a307253619c98fb
Inna_IT <leto2077@gmail.com>
2023-02-13 21:27:31 +0300

    задача про дни недели

---
+++ DZ-Csh2-zad3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день 
+// выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+System.Console.Write("Enter number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a == 6 || a == 7)
+{
+    System.Console.WriteLine("Этот день является выходным днем");
+}
+if (a >= 1 && a <= 5)
+{
+    System.Console.WriteLine("Этот день рабочий");
+}
+if (a > 7 || a <= 0)
+{
+    System.Console.WriteLine("Такого дня недели нет");
+}
